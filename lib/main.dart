44711d31a3b71ed8f564d5765c48589906c42193
Abo-ayman <jahizz2025@gmail.com>
2026-09-20 07:43:04 +0300

    Rename EWalletApp to ShamCashApp and update theme
    Updated the main app class and theme settings.

--- lib/main.dart
+++ lib/main.dart
@@ -1,54 +1,31 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:provider/provider.dart';
-import 'package:google_fonts/google_fonts.dart';
-
 import 'providers/wallet_provider.dart';
 import 'screens/main_navigation.dart';
 
 void main() {
-  runApp(const EWalletApp());
+  runApp(const ShamCashApp());
 }
 
-class EWalletApp extends StatelessWidget {
-  const EWalletApp({super.key});
+class ShamCashApp extends StatelessWidget {
+  const ShamCashApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final base = ThemeData.dark();
-
-    final theme = base.copyWith(
-      scaffoldBackgroundColor: const Color(0xFF020824),
-      textTheme: GoogleFonts.tajawalTextTheme(base.textTheme),
-      colorScheme: base.colorScheme.copyWith(
-        primary: const Color(0xFF0D47A1),
-        secondary: const Color(0xFF00BFA5),
-        surface: const Color(0xFF08112E),
-      ),
-      bottomNavigationBarTheme: const BottomNavigationBarThemeData(
-        backgroundColor: Color(0xFF08112E),
-        selectedItemColor: Colors.white,
-        unselectedItemColor: Colors.grey,
-      ),
-    );
-
     return ChangeNotifierProvider(
       create: (_) => WalletProvider(),
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
-        locale: const Locale('ar'),
-        supportedLocales: const [Locale('ar')],
-        localizationsDelegates: const [
-          GlobalMaterialLocalizations.delegate,
-          GlobalWidgetsLocalizations.delegate,
-          GlobalCupertinoLocalizations.delegate,
-        ],
-        builder: (context, child) => Directionality(
-          textDirection: TextDirection.rtl,
-          child: child!,
+        title: "ShamCash",
+        theme: ThemeData(
+          fontFamily: "Tajawal",
+          scaffoldBackgroundColor: const Color(0xFF0A122F),
+          colorScheme: ColorScheme.fromSeed(
+            seedColor: const Color(0xFF0A122F),
+            brightness: Brightness.dark,
+          ),
         ),
-        theme: theme,
-        home: const MainNavigationScreen(),
+        home: const MainNavigation(),
       ),
     );
   }
