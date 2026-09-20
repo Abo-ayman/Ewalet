9f7d26859fee19299b57c93cb8c336aef5cb7529
Abo-ayman <jahizz2025@gmail.com>
2026-09-20 15:07:07 +0300

    Refactor main.dart to use HomeScreen directly
    Removed provider and main navigation, updated home screen.

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'providers/wallet_provider.dart';
-import 'screens/main_navigation.dart';
+import 'screens/home_screen.dart';
 
 void main() {
   runApp(const ShamCashApp());
@@ -12,21 +10,12 @@ class ShamCashApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (_) => WalletProvider(),
-      child: MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: "ShamCash",
-        theme: ThemeData(
-          fontFamily: "Tajawal",
-          scaffoldBackgroundColor: const Color(0xFF0A122F),
-          colorScheme: ColorScheme.fromSeed(
-            seedColor: const Color(0xFF0A122F),
-            brightness: Brightness.dark,
-          ),
-        ),
-        home: const MainNavigation(),
+    return MaterialApp(
+      title: 'ShamCash',
+      theme: ThemeData(
+        primarySwatch: Colors.green,
       ),
+      home: const HomeScreen(),
     );
   }
 }
